0e27d8dea738bbbcf394252c0a4278f68bed8722
Adrian Cunningham <adrian.cunningham@gmail.com>
2013-10-27 12:58:43 -0300

    Implemented Disqus commenting system.

--- Oba30/Extensions.cs
+++ Oba30/Extensions.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Configuration;
+using Oba30.Infrastructure.Objects;
+using System.Web.Mvc;
 
 namespace Oba30
 {
@@ -10,6 +12,11 @@ namespace Oba30
             var istTZ = TimeZoneInfo.FindSystemTimeZoneById(ConfigurationManager.AppSettings["Timezone"]);
             return string.Format("{0} ({1})", TimeZoneInfo.ConvertTimeFromUtc(utcDT, istTZ).ToShortDateString(),
                 ConfigurationManager.AppSettings["TimezoneAbbr"]);
+        }
+
+        public static string Href(this Post post, UrlHelper helper)
+        {
+            return helper.RouteUrl(new { controller = "Blog", action = "Post", year = post.PostedOn.Year, month = post.PostedOn.Month, title = post.UrlSlug });
         }
     }
 }
